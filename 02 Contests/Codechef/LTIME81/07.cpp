05de1ff00a6b9a79f014b7f0c54184883e434cb9
yash0530 <yashjain0530@gmail.com>
2020-03-21 00:47:38 +0530

    .

--- 02 Contests/Codechef/LTIME81/07.cpp
+++ 02 Contests/Codechef/LTIME81/07.cpp
@@ -3,34 +3,112 @@ using namespace std;
 
 #define endl "\n"
 #define INF (int) 9e18
-#define HELL (int) (1e9 + 7)
 #define int long long
-#define double long double
-#define uint unsigned long long
-#define deb(x) cerr << #x << " => " << x << "\n"
-#define deba(x) cerr << #x << "\n"; for (auto a : x) cerr << a << " "; cerr << "\n";
-#define debm(x) cerr << #x << "\n"; for (auto a : x){for(auto b : a) cerr << b << " "; cerr << "\n";}
-#define getMat(x, n, m, val) vector<vector<int>> x(n, vector<int> (m, val))
 #define fastio ios_base :: sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-#define pout cout << fixed << setprecision(10)
 
-int fastpow(int a, int b, int m) {
-    int res = 1; a %= m; while (b > 0) {
-        if (b & 1) res = (res * a) % m;
-        a = (a * a) % m; b >>= 1;
-    } return res;
+const int maxN = 1e5 + 5, maxLG = 20;
+
+// dfs pre ordering
+int id;
+vector<vector<int>> adj;
+int dfs_in[maxN], vals[maxN], dfs_out[maxN], inv_dfs[maxN];
+
+// kth lca
+int LG;
+int up[maxN][maxLG];
+
+void dfs(int v = 1, int p = 1, int level = 0) {
+    dfs_in[v] = id;
+    vals[id] = level;
+    inv_dfs[id] = v;
+    id++;
+
+    up[v][0] = p;
+    for (int i = 1; i <= LG; ++i) {
+        up[v][i] = up[up[v][i-1]][i-1];
+    }
+
+    for (auto x : adj[v]) {
+        if (x != p) {
+            dfs(x, v, level + 1);
+        }
+    }
+    dfs_out[v] = id - 1;
 }
 
-int now() {
-    return chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now()
-    .time_since_epoch()).count();
+int kthLCA(int node, int k) {
+    for (int i = 0; i <= LG; i++) {
+        if ((1 << i) & k) {
+            node = up[node][i];
+        }
+    }
+    return node;
+}
+
+// segment tree
+int n; pair<int, int> tree[4 * maxN];
+stack<pair<int, pair<int, int>>> changes;
+
+void build(int a[], int v, int tl, int tr) {
+    if (tl == tr) {
+        tree[v] = { a[tl], tl };
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        tree[v] = max(tree[v*2], tree[v*2+1]);
+    }
+}
+
+void update(int v, int tl, int tr, int l, int r) {
+    if (l > r) 
+        return;
+    if (l == tl and tr == r) {
+        changes.push({ v, tree[v] });
+        tree[v] = { -1, INF };
+        return;
+    } else {
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm));
+        update(v*2+1, tm+1, tr, max(l, tm+1), r);
+        changes.push({ v, tree[v] });
+        tree[v] = max(tree[v*2], tree[v*2+1]);
+    }
 }
 
 int32_t main() { fastio;
-    time_t start = now();
+    int t; cin >> t;
+    while (t--) {
 
-    
+        cin >> n; LG = log2(ceil(n));
+        adj = vector<vector<int>> (n + 1);
+        for (int i = 0; i < n - 1; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        id = 0;
 
-    cerr << "TIME => " << now() - start << endl;
+        dfs();
+        build(vals, 1, 0, n - 1);
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            auto x = tree[1];
+            int alpha = 0, count = 0;
+            while (x.first >= 0) {
+                int node = kthLCA(inv_dfs[x.second], i);
+                update(1, 0, n - 1, dfs_in[node], dfs_out[node]);
+                x = tree[1];
+                ans++;
+            }
+            while (!changes.empty()) {
+                tree[changes.top().first] = changes.top().second;
+                changes.pop(); 
+            }
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
--- 06 Helper Codes/test.py
+++ 06 Helper Codes/test.py
@@ -1,1 +1,16 @@
-print (500)
+from random import seed
+from random import random
+import time
+import math
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+seed(current_milli_time)
+
+count = 0
+val = 10000000
+for i in range(val):
+    x = int(random() * val)
+    y = int(random() * val)
+    if (math.gcd(x, y) == 1):
+        count += 1
+print(math.sqrt(6 / (count / val)))
