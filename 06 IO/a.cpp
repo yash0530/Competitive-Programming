56526095fa65c13d9a77c331ce9125987cab44df
Yash Jain <yashjain0530@gmail.com>
2020-11-11 18:46:40 +0530

    updated build

--- 06 IO/a.cpp
+++ 06 IO/a.cpp
@@ -1,35 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int getDefiniteFinds(vector<int> &arr) {
-    int n = arr.size();
-
-    // suffix_min denotes min (i...n)
-    vector<int> suff_min(n + 1);
-    suff_min[n] = INT_MAX;
-    for (int i = n - 1; i >= 0; i--) {
-        suff_min[i] = min(suff_min[i + 1], arr[i]);
-    }
-
-    // mn denotes max(0...i-1)
-    int mn = INT_MIN, ans = 0;
-    for (int i = 0; i < n; i++) {
-        // if max element till i-1 is less than element at i
-        // if min element from i+1 to n is greater than element at i
-        // the element will surely be found
-        if (mn < arr[i] and arr[i] < suff_min[i + 1]) {
-            ans++;
-        }
-
-        // maintaing mn
-        mn = max(arr[i], mn);
-    }
-    return ans;
-}
-
 int main() {
-	int n; cin >> n;
-	vector<int> arr(n);
-	for (auto &a : arr) cin >> a;
-    cout << getDefiniteFinds(arr) << endl;
+    int t; cin >> t;
+    cout << "T: " << t << endl;
 }
